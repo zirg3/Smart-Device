0631a414b4beafcb28bc6a62e43b195c5dbefbda
zirg3 <zirg31@gamil.com>
2019-08-08 13:40:20 +0300

    Футер и попап

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,30 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+let write = document.querySelector(".page-header__call");
+let popup = document.querySelector(".popup-feedback");
+let close = document.querySelector(".popup-feedback__svg-close");
+let scrollHid = document.querySelector(".scroll-vissible");
+let back = document.querySelector(".popup-wrap");
 
-pageHeader.classList.remove('page-header--nojs');
+write.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-feedback__show");
+  scrollHid.classList.add("scroll-hidden");
+  back.classList.add("popup-wrap--open");
+});
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("popup-feedback__show");
+  scrollHid.classList.remove("scrol-hidden");
+  back.classList.remove("popup-wrap--open");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("popup-feedback__show")) {
+      popup.classList.remove("popup-feedback__show");
+      scrollHid.classList.remove("scroll-hidden");
+      back.classList.remove("popup-wrap--open");
+    }
   }
 });
